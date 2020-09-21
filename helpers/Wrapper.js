11d680978b07f671271dc32fd1c88cabc3b6859f
viktorholk <viktorholk@gmail.com>
2020-09-21 14:57:49 +0200

    ScriptHandler progress and rewriting of the project - Fix Wrapper is not a constructor

--- helpers/Wrapper.js
+++ helpers/Wrapper.js
@@ -1,40 +1,114 @@
 //Module to work with folders and files
 const fs = require('fs');
 const path = require('path');
-// Config
-const config = require('../config.json');
+const Logger = require('./Logger');
+const logger = new Logger().Instance();
+
+const Script = require('./Script');
+const ScriptHandler = require('./ScriptHandler');
 
 class Wrapper {
-
     constructor(){
-        this.metadata = {
-            enabled: false,
-            script: '',
-            scriptCommand: '',
-            timeOut: 30,
-            cost: 0,
-            followerOnly: true,
-            subscriberOnly: false,
-            modOnly: false
+        this.scriptsPath    = 'scripts'
+        this.configPath     = 'config.json'
+        /* 
+            Check if all the nescessary folders and files exists
+        */
+        // Scripts
+        this.CreateFolder(this.scriptsPath);
+        // Config
+        // Only create the config file if it doesn't exist already, since we dont want to override the config later
+        if (!fs.existsSync(this.configPath)){
+            this.WriteJson(this.configPath,{
+                username: 'tactoctical',
+                token:    'oauth:3amvpcglyxc1xxqlr8iirm1no524i3',
+                prefix: '/',
+                error_messages:{
+                    followerError: 'You have to be a follower to use this command',
+                    subscriberError: 'You have to be a subscriber to use this command',
+                    modError: 'You have to be a mod to use this command'
+                },
+                execute_config:[
+                    {
+                        name: "python",
+                        ext: ".py",
+                        shell: "python "
+                    }
+                ],
+                scripts:[]
+            });
         }
+       }
 
-        /*
-            Create all the neceesary folders
-        */
-       for (var i in config.paths){
-            if (!fs.existsSync(i)){
-                fs.mkdirSync(i);
-            }
+    CreateFolder(_path){
+        if (!fs.existsSync(_path)){
+            fs.mkdir(_path, (err) => { 
+                if (err) { 
+                    logger.Log(err,3); 
+                } 
+                logger.Log(_path + ' folder created successfully!', 2); 
+            }); 
         }
-        /*
-            Check all current files and if no metadata file add one.
-        */
-       fs.readdirSync(config.paths['scripts']).forEach(file => {
-            this.CreateMetaData(file);
-        })
     }
 
+    ReadJson(_path){
+        if (fs.existsSync(_path)){
+            try{
+                return JSON.parse(fs.readFileSync(_path))
+            }
+            catch (err){
+                logger.Log(err,3);
+            }
+        }
+        logger.Log(_path + ' Could not read', 3);
+        return null;
+    }
 
+    WriteJson(_path, data){
+        try{
+            fs.writeFileSync(_path, JSON.stringify(
+                data, null, 4
+            ));
+            logger.Log(_path + ' Wrote ' + data, 2)
+        }
+        catch (err){
+            logger.Log(err,3);
+        }
+    }
+    /**
+     * Check if all the files in scripts folder is related to a metadata json in the config and the other way around
+     */
+    
+    // ValidateScripts(){
+    //     let _config = this.ReadJson(this.configPath);
+    //     let files = fs.readdirSync(this.scriptsPath);
+
+    //     for (let i in files){
+    //         let _file = files[i];
+    //         // Compare all file names to script names in config
+    //         let exists = false;
+    //         for (let j in _config['scripts']){
+    //             if (_config['scripts'][j]['script'] === _file){
+    //                 exists = true;
+    //             }
+    //         }
+    //         if (!exists){
+
+    //         }
+    //     } 
+
+    //     for (let i in _config['scripts']){
+    //         let _script = _config['scripts'][i]['script'];
+
+    //         for (let j in files){
+    //             let _file = files[j];
+    //             if (_script === _file){
+    //                 continue;
+    //             }
+    //         }
+    //         //_obj = new ScriptHandler(new Script(_file)).RemoveScript();
+    //     }
+    // }
 }
 
 class Singleton{
