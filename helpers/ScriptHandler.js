eb5929f57e4870fc129a161bac271cd7fff00bcc
viktorholk <viktorholk@gmail.com>
2020-09-23 15:03:04 +0200

    ScriptHandler progress and rewriting of the project - Fix Wrapper is not a constructor

--- helpers/ScriptHandler.js
+++ helpers/ScriptHandler.js
@@ -1,56 +1,72 @@
-const fs = require('fs');
 const path = require('path');
-const MetadataHandler = require('./MetadataHandler');
-const { exec } = require('child_process');
-const config = require('../config.json');
-const { stderr } = require('process');
+const { exec } = require("child_process");
+const Wrapper = require('./Wrapper')
+const Logger = require('./Logger');
+const logger = new Logger().Instance();
+const wrapper = new Wrapper().Instance();
+
 class ScriptHandler{
     constructor(_script){
-        this.script = _script
-        this.ext = path.extname(_script);
-        this.handler = new MetadataHandler().Instance();
-
-        let _config          = this.handler.ReadMetadata();
-        this._execute_config = _config['execute_config'];
-        this._scripts_config = _config['scripts']
+        this.scriptObject   = _script;
+        this.scriptExt      = path.extname(_script['script']);
     }
 
     Execute(){
-        // Fint the metadatafile to the _script
-        let _execute_ext;
-        for (let i in this._scripts_config){
-            if (this._scripts_config[i]['script'] === this.script){
-                console.log('* Metadata exists')
-                _execute_ext = path.extname(this._scripts_config[i]['script']);
+        
+        let _config = wrapper.ReadJson(wrapper.configPath);
+        // Check if script exists in [scripts]
+        let exists = false;
+        for (let i in _config['scripts']){
+            if (_config['scripts'][i]['script'] === this.scriptObject['script']){
+                exists = true
             }
         }
-        if (!_execute_ext) {
-            console.log('* Metadata does not exist');
+        if (!exists) this.AppendScript();
+        //Read config and see what extensions are avaiable
+        let method = null;
+        for (let i in _config['execute_config']){
+            if (_config['execute_config'][i]['ext'] === this.scriptExt){
+                method = _config['execute_config'][i];
+            }
+        }
+        if (!method){
+            logger.Log('Not a valid script ' + this.scriptExt);
             return;
         }
-       
-        switch (_execute_ext){
-            case '.py':
-                    exec('python  ' +  path.join('scripts', this.script), (err, stdout, stderr) => {
-                        if (err){
-                            console.log(err);
-                        }else{
-                            this.ColorPrint(stdout);
-                        }
-                    }) ;
-                break;
 
-            default:
-                console.log('Extension ' + _execute_ext + ' is not allowed.');
-                break;
+        logger.Log('Executing ' + this.scriptObject['script'], 2);
+    }
+
+    AppendScript(){
+        let _config = wrapper.ReadJson(wrapper.configPath);
+        // Check if script already exists
+        for (let i in _config['scripts']){
+            if (_config['scripts'][i]['script'] === this.scriptObject['script']){
+                logger.Log(this.scriptObject['script'] + ' already exists', 3)
+                return;
+            }
         }
+        _config['scripts'].push(this.scriptObject);
+        wrapper.WriteJson(wrapper.configPath, _config);
+        logger.Log('Added ' + this.scriptObject['script'] + ' metadata to [scripts]', 2);
     }
 
-    ColorPrint(msg){
-        console.log('\x1b[35m' + msg + '\x1b[0m');
+    RemoveScript(){
+        let _config = wrapper.ReadJson(wrapper.configPath);
+        for (let i in _config['scripts']){
+            if (_config['scripts'][i]['script'] === this.scriptObject['script']){
+                const index =  _config['scripts'].indexOf(_config['scripts'][i]);
+                if (index > -1){
+                    _config['scripts'].splice(index,1)
+                }
+
+                wrapper.WriteJson(wrapper.configPath, _config);
+                logger.Log('Removed ' + this.scriptObject['script'] + ' from metadata [scripts]',2);
+                return;
+            }
+        }
+        logger.Log('Could not find ' + this.scriptObject['script'] + ' in [scripts]');
     }
-
-
-
 }
+
 module.exports = ScriptHandler;
