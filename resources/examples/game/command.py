01492971e275b775ae3072e211cf12b06ef618ca
viktorholk <viktorholk@gmail.com>
2020-10-23 11:36:52 +0200

    working keypress for games

--- resources/examples/game/command.py
+++ resources/examples/game/command.py
@@ -2,74 +2,111 @@ from sys import argv
 from os import mkdir, path
 from time import sleep
 import json
-import win32api
-import win32con
+import ctypes
 
-VK_CODE = {'backspace':0x08,
-           'tab':0x09,
-           'clear':0x0C,
-           'enter':0x0D,
-           'shift':0x10,
-           'ctrl':0x11,
-           '0':0x30,
-           '1':0x31,
-           '2':0x32,
-           '3':0x33,
-           '4':0x34,
-           '5':0x35,
-           '6':0x36,
-           '7':0x37,
-           '8':0x38,
-           '9':0x39,
-           'a':0x41,
-           'b':0x42,
-           'c':0x43,
-           'd':0x44,
-           'e':0x45,
-           'f':0x46,
-           'g':0x47,
-           'h':0x48,
-           'i':0x49,
-           'j':0x4A,
-           'k':0x4B,
-           'l':0x4C,
-           'm':0x4D,
-           'n':0x4E,
-           'o':0x4F,
-           'p':0x50,
-           'q':0x51,
-           'r':0x52,
-           's':0x53,
-           't':0x54,
-           'u':0x55,
-           'v':0x56,
-           'w':0x57,
-           'x':0x58,
-           'y':0x59,
-           'z':0x5A,
+# These are direct input scan codes. Which means that these keys will be able to be used in games, since the games except a different key code then we usually uses
+KEY_CODES = {
+"ESC":      0x01,
+"ENTER":    0x1C,
+"TAB":      0x0F,
+"1":        0x02,
+"2":        0x03,
+"3":        0x04,
+"4":        0x05,
+"5":        0x06,
+"6":        0x07,
+"7":        0x08,
+"8":        0x09,
+"9":        0x0A,
+"0":        0x0B,
+"Q":        0x10,
+"W":        0x11,
+"E":        0x12,
+"R":        0x13,
+"T":        0x14,
+"Y":        0x15,
+"U":        0x16,
+"I":        0x17,
+"O":        0x18,
+"P":        0x19,
+"A":        0x1E,
+"S":        0x1F,
+"D":        0x20,
+"F":        0x21,
+"G":        0x22,
+"H":        0x23,
+"J":        0x24,
+"K":        0x25,
+"L":        0x26,
+"Z":        0x2C,
+"X":        0x2D,
+"C":        0x2E,
+"V":        0x2F,
+"B":        0x30,
+"N":        0x31,
+"M":        0x32,
+".":        0x33,
+",":        0x34,
+"/":        0x35,
+" ":        0x39
 }
+
+SendInput = ctypes.windll.user32.SendInput
+
+# C Struct redifinitions
+PUL = ctypes.POINTER(ctypes.c_ulong)
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
+
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time",ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput),
+                 ("mi", MouseInput),
+                 ("hi", HardwareInput)]
+
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
+
+# Actuals Functions
+def PressKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def ReleaseKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+
 
 CONFIG_FOLDER   = 'config'
 COMMANDS_JSON   = path.join(CONFIG_FOLDER, 'commands.json')
 SLEEP_TIME      = 0.15
 
-def press(key):
-    win32api.keybd_event(VK_CODE[key], 0,0,0)
-    sleep(.15)
-    win32api.keybd_event(VK_CODE[key],0, win32con.KEYEVENTF_KEYUP, 0)
-
-def typer(_str):
-    pass
 
 if __name__ == "__main__":
-
-    while True:
-        win32api.keybd_event(0x57, 0,0,0)
-        sleep(1)
-
-
-    exit(0)
-
     # Create necessary folder and files
     # * Config [ FOLDER ]
     #   * - commands [ JSON ]  - store the commands here
@@ -111,7 +148,7 @@ if __name__ == "__main__":
         print(f'Typing {_cmd} command')
         
         #Press the chat key
-        press(chat_key)
+        PressKey(KEY_CODES['T'])
 
         
         # # Type
